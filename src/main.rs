2b4eefa04698af26e558cbd3abef6090ff4cbe0d
Andy Isaacson <adi@hexapodia.org>
2020-08-30 21:32:19 -0700

    implement short_hash and put the block hashes of the file in a Vec

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate sha2;
 use getopts::Options;
 use std::env;
 
+use std::convert::TryInto;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io;
@@ -16,7 +17,6 @@ pub struct Sha {
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    //let program = args[0].clone();
 
     let mut opts = Options::new();
     opts.optopt("o", "output", "set output file name", "NAME");
@@ -31,12 +31,33 @@ fn main() {
     println!("{}", h.hexdigest())
 }
 
+const BUFFER_SIZE : usize = 4096;
+
+fn short_hash(buf : &[u8]) -> u32 {
+    let mut h = Sha256::new();
+    h.input(&buf);
+    let r = h.result();
+    let (int_bytes, _) = r.split_at(std::mem::size_of::<u32>());
+    return u32::from_be_bytes(int_bytes.try_into().unwrap())
+}
+
 fn hash_file(fname: String) -> io::Result<Sha> {
     let mut f = File::open(fname)?;
     let mut h = Sha::new();
-    let mut buf = Vec::new();
-    f.read_to_end(&mut buf)?;
-    h.update(&buf);
+    let mut buf = [0u8; BUFFER_SIZE];
+    let mut v = Vec::new();
+    loop {
+        let n = match f.read(&mut buf) {
+            Ok(n) => { n }
+            Err(e) => return Err(e)
+        };
+        h.update(&buf[..n]);
+        let h0 = short_hash(&buf[..n]);
+        v.push(h0);
+        if n == 0 || n < BUFFER_SIZE {
+            break;
+        }
+    }
     Ok(h)
 }
 
